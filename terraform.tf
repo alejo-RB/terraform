134bf005d52281f97a03c91db0fa286d29f3c56e
alejo-RB <rodrgueza@yahoo.es>
2023-02-02 02:00:05 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -91,7 +91,7 @@ resource "azurerm_virtual_machine" "test_cloudazure" {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
-    version   = "latest_cloudazure"
+    version   = "latest"
   }
 
   storage_os_disk {

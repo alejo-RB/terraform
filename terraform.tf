248e7d34c0722190afb81da6a986f488a4836b5b
alejo-RB <rodrgueza@yahoo.es>
2023-02-13 16:46:53 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,4 @@
+# Configure the Azure provider
 terraform {
 
   required_version = ">=0.12"
@@ -15,10 +16,11 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "test_cloudazure" {
-  name     = "prueba"
+  name     = "Arquitectura-Cloud"
   location = "West US 3"
 }
 
+# Red Virtual
 resource "azurerm_virtual_network" "test_cloudazure" {
   name                = "network_vm"
   address_space       = ["10.0.0.0/16"]
@@ -26,13 +28,15 @@ resource "azurerm_virtual_network" "test_cloudazure" {
   resource_group_name = azurerm_resource_group.test_cloudazure.name
 }
 
+# SubNet
 resource "azurerm_subnet" "test_cloudazure" {
-  name                 = "net_private"
+  name                 = "SubNet_private"
   resource_group_name  = azurerm_resource_group.test_cloudazure.name
   virtual_network_name = azurerm_virtual_network.test_cloudazure.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# IP Publica
 resource "azurerm_public_ip" "test_cloudazure" {
   count               = 2
   name                = "publicIP-${count.index}"
@@ -40,12 +44,33 @@ resource "azurerm_public_ip" "test_cloudazure" {
   resource_group_name = azurerm_resource_group.test_cloudazure.name
   allocation_method   = "Static"
   sku                 = "Standard"
-
-  dns_settings {
-    domain_name_label = "machinerb${count.index}"
-  }
+  domain_name_label   = "machinerb${count.index}"
 }
 
+# Servidor de Base de Datos
+resource "azurerm_mssql_server" "ServerBD" {
+  name                         = "serverbd3000"
+  location                     = azurerm_resource_group.ArquitecturaCloud.location
+  resource_group_name          = azurerm_resource_group.ArquitecturaCloud.name
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "SQLpassword123!"
+}
+
+# Bases de Datos
+resource "azurerm_mssql_database" "sql-db" {
+  name                 = "SQL-Database"
+  server_id            = azurerm_mssql_server.ServerBD.id
+  create_mode          = "Default"
+}
+
+resource "azurerm_mssql_database" "sql-wordpress" {
+  name                 = "Database-Wordpress"
+  server_id            = azurerm_mssql_server.ServerBD.id
+  create_mode          = "Default"
+}
+
+# Interfaz de Red
 resource "azurerm_network_interface" "test_cloudazure" {
   count               = 2
   name                = "net${count.index}"
@@ -56,9 +81,10 @@ resource "azurerm_network_interface" "test_cloudazure" {
     name                          = "test_cloudazureConfiguration"
     subnet_id                     = azurerm_subnet.test_cloudazure.id
     private_ip_address_allocation = "static"
-    private_ip_address            = "${cidrhost("10.0.2.0/24",  4+count.index)}"
+    private_ip_address            = cidrhost("10.0.2.0/24", 4 + count.index)
     public_ip_address_id          = azurerm_public_ip.test_cloudazure[count.index].id
   }
+
 }
 
 resource "azurerm_managed_disk" "test_cloudazure" {
@@ -78,6 +104,13 @@ resource "azurerm_availability_set" "avset" {
   platform_fault_domain_count  = 2
   platform_update_domain_count = 2
   managed                      = true
+}
+
+
+# Variable para el directorio de clave privada
+variable "private_key_path" {
+  type    = string
+  default = "C:\\terraform\\keys\\private"
 }
 
 resource "azurerm_virtual_machine" "test_cloudazure" {
@@ -136,15 +169,25 @@ resource "azurerm_virtual_machine" "test_cloudazure" {
     disable_password_authentication = false
   }
 
+  # Conexion Clave Privada
+  connection {
+    type        = "ssh"
+    user        = "alejo"
+    password    = "Alejo2001."
+    private_key = file(var.private_key_path)
+    host        = azurerm_network_interface.test_cloudazure.id
+  }
+
   tags = {
     environment = "staging"
   }
 }
 
+
 #crear grupo de seguridad y reglas de firewall
 
-resource "azurerm_network_security_group" "nsg" {
-  name                = "ssh_nsg"
+resource "azurerm_network_security_group" "reglas" {
+  name                = "ssh_reglas"
   location            = azurerm_resource_group.test_cloudazure.location
   resource_group_name = azurerm_resource_group.test_cloudazure.name
 
@@ -172,10 +215,42 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 }
+
 #asociacion de grupo de seguridad con las maquinas virtuales existentes
 resource "azurerm_network_interface_security_group_association" "association" {
+
   count = length(azurerm_network_interface.test_cloudazure)
 
   network_interface_id      = azurerm_network_interface.test_cloudazure[count.index].id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+  network_security_group_id = azurerm_network_security_group.reglas.id
 }
+
+#Instalación de docker y docker compose
+
+
+#Sistema de monitoreo
+#Logs por medio de consultas
+resource "azurerm_log_analytics_workspace" "test_cloudazure" {
+
+  count = length(azurerm_network_interface.test_cloudazure)
+
+  name                = "example-log-analytics-workspace-${count.index}"
+  location            = azurerm_resource_group.test_cloudazure.location
+  resource_group_name = azurerm_resource_group.test_cloudazure.name
+
+  sku = "PerGB2018"
+}
+
+resource "azurerm_storage_account" "test_cloudazure" {
+
+  count = length(azurerm_network_interface.test_cloudazure)
+
+  name                     = "exampleaccount${count.index}"
+  resource_group_name     = azurerm_resource_group.test_cloudazure.name
+  location                 = azurerm_resource_group.test_cloudazure.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+
+
